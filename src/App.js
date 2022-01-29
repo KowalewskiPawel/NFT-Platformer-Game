b68bd19f8598e5498eca62c075d776709605f9f7
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-01-29 11:31:05 +0100

    inital version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Phaser from 'phaser'
+import { IonPhaser } from '@ion-phaser/react'
 
-function App() {
+import BackgroundImage from './assets/BG.png';
+
+const game = {
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  scene: {
+    preload: function() {
+      this.load.image('background', BackgroundImage);
+    },
+    create: function() {
+      this.add.image(400, 300, 'background').setScale(0.55);
+    },
+    update: function() {
+    }
+  }
+}
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonPhaser game={game} />
   );
 }
 
