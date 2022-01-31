dfb5d7e822edf01071c40846fd146a2fbf3d4ae5
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-01-31 18:51:16 +0100

    tile added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import Phaser from 'phaser'
 import { IonPhaser } from '@ion-phaser/react'
 
 import BackgroundImage from './assets/BG.png';
+import tile from './assets/tile.png';
+
+let platforms;
 
 const game = {
   width: 800,
   height: 600,
   type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
   scene: {
     preload: function() {
       this.load.image('background', BackgroundImage);
+      this.load.image('tile', tile);
+      
     },
     create: function() {
       this.add.image(400, 300, 'background').setScale(0.55);
+      platforms = this.physics.add.staticGroup();
+      platforms.create(600, 400, 'tile').setScale(0.5).refreshBody();
     },
     update: function() {
     }
